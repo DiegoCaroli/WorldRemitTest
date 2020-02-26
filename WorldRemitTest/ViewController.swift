92e7bb3f85052238826419de79944cf7b767a034
Diego Caroli <diego@vegans.it>
2020-02-26 14:40:09 +0100

    Initial Commit

---
+++ WorldRemitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldRemitTest
+//
+//  Created by Diego Caroli on 26/02/2020.
+//  Copyright © 2020 Diego Caroli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
